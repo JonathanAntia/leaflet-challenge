d2630c310ff59381c23314bfde70b3df3a20e7e4
JonathanAntia <a_jon12@yahoo.com>
2020-09-19 14:24:28 -0500

    level 2 javascript app

--- static/js/level2.js
+++ static/js/level2.js
@@ -30,23 +30,11 @@ const satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{
     accessToken: API_KEY
 })
 
-// create a map object
-const myMap = L.map('map', {
-    center: [37.77,-122.42],
-    zoom:4,
-    layers: [dark]
-});
-
 const baseMaps = {
     "Dark": dark,
     "Light": light,
     "Satellite": satellite
 };
-
-L.control.layers(baseMaps).addTo(myMap);
-
-// link to USGS earthquake data
-const earthquakeDataURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson';
 
 // change the color depending on the significance of each earthquake
 function chooseColor(sig){
@@ -73,53 +61,8 @@ function convertDate(time){
     return date;
 };
 
-// retrieve the earthquake data and add each to the map
-d3.json(earthquakeDataURL).then(function(data){
-    console.log(data);
-    let earthquakes = data.features;
-    earthquakes.forEach(earthquake=>{
-        const date = convertDate(earthquake.properties.time);
-        if (earthquake.geometry.coordinates){
-            const mag = L.circle([earthquake.geometry.coordinates[1],earthquake.geometry.coordinates[0]],{
-                color: 'blank',
-                fillColor: chooseColor(earthquake.properties.sig),
-                fillOpacity: 0.75,
-                radius: earthquake.properties.mag*10000
-            }).bindPopup(`<div class='info'>${date}<br>Depth: ${earthquake.geometry.coordinates[2]} km</div>`)
-            .addTo(myMap);   
-        }
-    })
-    // create a legend
-    const legend = L.control({ position: "bottomright" });
-
-    legend.onAdd = function(map) {
-    var div = L.DomUtil.create("div", "legend");
-    div.innerHTML += "<h4>EQ Significance</h4>";
-    div.innerHTML += '<i style="background: #FFD1A9"></i><span>0-250</span><br>';
-    div.innerHTML += '<i style="background: #FF9E79"></i><span>250-500</span><br>';
-    div.innerHTML += '<i style="background: #FB6D4C"></i><span>500-750</span><br>';
-    div.innerHTML += '<i style="background: #C23B22"></i><span>750-1000</span><br>';
-    div.innerHTML += '<i style="background: #8A0000"></i><span>1000+</span><br>';
-    return div;
-    };
-
-legend.addTo(myMap);
-});
-
 // link to geojson fault data
 const faultData = 'static/data/qfaults_latest_quaternary.geojson';
-
-// change the stroke weight depending on slip_rate of each fault
-function strokeWeight (slip_rate){
-    switch(slip_rate){
-        case "Greater than 5.0 mm/yr":
-            return 1;
-        case "Between 1.0 and 5.0 mm/yr":
-            return 0.5;
-        default:
-            return 0;
-    }
-}
 
 // retrieve the fault data and add each to the map
 d3.json(faultData).then(function(data){
@@ -127,9 +70,72 @@ d3.json(faultData).then(function(data){
     const faults = L.geoJson(data, {
         style: function(feature){
             return{
-                color: 'white',
+                color: 'green',
                 weight: strokeWeight(feature.properties.slip_rate)
             }
         }
-    }).addTo(myMap);
+    })
+            
+    // create a map object
+    const myMap = L.map('map', {
+        center: [37.77,-122.42],
+        zoom:4,
+        layers: [dark, faults]
+    });
+
+    // link to USGS earthquake data
+    const earthquakeDataURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson';
+
+    // retrieve the earthquake data and add each to the map
+    d3.json(earthquakeDataURL).then(function(data){
+        console.log(data);
+        let earthquakes = data.features;
+        earthquakes.forEach(earthquake=>{
+            const date = convertDate(earthquake.properties.time);
+            if (earthquake.geometry.coordinates){
+                L.circle([earthquake.geometry.coordinates[1],earthquake.geometry.coordinates[0]],{
+                    color: 'blank',
+                    fillColor: chooseColor(earthquake.properties.sig),
+                    fillOpacity: 0.75,
+                    radius: earthquake.properties.mag*10000
+                }).bindPopup(`<div class='info'>${date}<br>Depth: ${earthquake.geometry.coordinates[2]} km</div>`)
+                .addTo(myMap);   
+            }
+        })
+
+        // create a legend
+        const legend = L.control({ position: "bottomright" });
+
+        legend.onAdd = function(map) {
+        var div = L.DomUtil.create("div", "legend");
+        div.innerHTML += "<h4>EQ Significance</h4>";
+        div.innerHTML += '<i style="background: #FFD1A9"></i><span>0-250</span><br>';
+        div.innerHTML += '<i style="background: #FF9E79"></i><span>250-500</span><br>';
+        div.innerHTML += '<i style="background: #FB6D4C"></i><span>500-750</span><br>';
+        div.innerHTML += '<i style="background: #C23B22"></i><span>750-1000</span><br>';
+        div.innerHTML += '<i style="background: #8A0000"></i><span>1000+</span><br>';
+        return div;
+        };
+
+        const overlays = {
+            'Faults': faults,
+        };
+        
+        L.control.layers(baseMaps, overlays).addTo(myMap);
+        legend.addTo(myMap);
+    });
 });
+
+
+
+// change the stroke weight depending on slip_rate of each fault
+function strokeWeight (slip_rate){
+    switch(slip_rate){
+        case "Greater than 5.0 mm/yr":
+            return 1.5;
+        case "Between 1.0 and 5.0 mm/yr":
+            return 1;
+        default:
+            return 0;
+    }
+};
